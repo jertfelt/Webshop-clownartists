3f311c1839bf54cb39685e028ce3db52cc23da32
Tova Jertfelt <tovizitan@gmail.com>
2022-01-17 21:10:04 +0100

    added mobile, finally fixed last bugs with shop. NEXT: product page and create your own

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-//variables
+//------menu
+
+
+
+
+//-----------variables
 
 let cartButt = document.querySelector('.cart-btn');
 let closeCartButt = document.querySelector('.close-cart');
@@ -11,15 +16,15 @@ let cartItems = document.querySelector(".cart-items");
 let cartTotal = document.querySelector(".cart-total");
 let cartContent = document.querySelector(".cart-content");
 
+
 //products
 let productsMenu = document.querySelector(".products-center");
 
 //cart
 let cart = [];
-let allCartButtons = [];
-let allMenuButtons = [];
+let buttons = [];
 
-
+//------------CLASSES
 //getting products from json 
 class Products{
   async getProducts(){
@@ -75,17 +80,16 @@ products.forEach(product => {
 productsMenu.innerHTML = result;
 }
 
-//buttons and adding to cart functionality
 getButtons(){
   //creating nodes to arrays so I can loop through them and use array-methods:
   let buttonsCart = [...document.querySelectorAll(".bag-btn")];
-  allCartButtons = buttonsCart;
+  buttons = buttonsCart;
 
-//*alternative buttons:
+  //alternative buttons:
    // let buttonsMenu = [...document.querySelectorAll(".grid-btn")];
   // allMenuButtons = buttonsMenu;
  
-  //getting the ID:s and checking if they are in the cart
+  //*getting the ID:s and checking if they are in the cart
   buttonsCart.forEach(button => {
 
     let id = button.dataset.id;
@@ -105,7 +109,6 @@ getButtons(){
         event.target.disabled = true;
 
         //product from products
-        //creating object:
         let cartItem = {...Storage.getProduct(id), amount:1};
         
         //add product to the cart (in array)
@@ -142,10 +145,10 @@ getButtons(){
   }
   //for each cart item:
   addCartItem(item){
-    let cartdiv =document.createElement("div");
-    cartdiv.classList.add("cart-item");
+    let cartdiv = document.createElement("div");
+    cartdiv.classList.add("cart-item-new");
     cartdiv.innerHTML = `
-    <div class="cart-item">
+    <div class="cart-item" data-id=${item.id}>
         <img src=${item.image}
     alt="product" />
     <div>
@@ -157,8 +160,9 @@ getButtons(){
       <i class="fas fa-chevron-up" data-id=${item.id}></i>
       <p class="item-amount">${item.amount}</p>
       <i class="fas fa-chevron-down"data-id=${item.id} ></i>
-    </div>
+    
     </div> 
+    </div>
     `
     cartContent.appendChild(cartdiv);
   }
@@ -191,17 +195,60 @@ getButtons(){
     }
 
   cartLogic(){
-    //clear cart (accessing within class)
+    //clear cart (whole cart)
     clearCartButt.addEventListener("click", () => {
       this.clearCart();
     });
 
-    //cart functionality (remove, add, take away)
-  
-        
-    
+    //cart functionality (remove , add, take away ONE ITEM) 
+    cartContent.addEventListener("click",event => {
+
+      if(event.target.classList.contains("remove-item")){
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        let removingDom = removeItem.parentElement.parentElement;
+        removingDom.classList.add("hide-item");
+        this.removeItem(id);
+      }
+
+      else if (event.target.classList.contains("fa-chevron-up")){
+       let addAmount = event.target;
+       let addId = addAmount.dataset.id;
+        //push new amount into local storage:
+        let temporaryItem = cart.find(item => item.id === addId);
+        temporaryItem.amount = temporaryItem.amount + 1;
+       
+        Storage.saveCart(cart);
+        this.setCartValue(cart);
+        addAmount.nextElementSibling.innerText = temporaryItem.amount;
+      }
+
+      else if (event.target.classList.contains("fa-chevron-down")){
+       let reduceAmount = event.target;
+       let reduceId = reduceAmount.dataset.id;
+       
+       //reduce new amount:
+       let temporaryItem = cart.find(item => item.id ===reduceId);
+       temporaryItem.amount = temporaryItem.amount -1;
       
-      
+        if(temporaryItem.amount > 0){
+          Storage.saveCart(cart);
+          this.setCartValue(cart);
+          reduceAmount.previousElementSibling.innerText = temporaryItem.amount;
+          
+        }
+        else{
+          console.log(reduceAmount.parentElement.parentElement.classList)
+          console.log(temporaryItem.id);
+           let classID = (reduceAmount.parentElement.parentElement.getAttribute("data-id"));
+          console.log(classID);
+          if (temporaryItem.id === classID){
+            reduceAmount.parentElement.parentElement.classList.add("hide-item")
+          }
+          this.removeItem(temporaryItem.id);
+        }
+      }
+    });
     }
   
 
@@ -215,9 +262,9 @@ getButtons(){
     while(cartContent.children.length>0){
       cartContent.removeChild(cartContent.children[0]);
     }
-    this.hideCart();
-    
+    this.hideCart(); 
   }
+
   removeItem(id){
     cart = cart.filter(item => item.id !== id);
     this.setCartValue(cart);
@@ -231,7 +278,7 @@ getButtons(){
 
   }
   getSingleCartButton(id){
-    return allCartButtons.find(button => button.dataset.id === id);
+    return buttons.find(button => button.dataset.id === id);
   };
 
 
